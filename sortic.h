4e51b05e59546e0483d69cd43773408812d9bbcc
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2022-01-25 13:54:07 +0300

    Update sortic.h

--- sortic.h
+++ sortic.h
@@ -7,13 +7,19 @@
 
 using namespace std;
 
-void sa(vector <int> a);
-void sb(vector <int> b);
-void ss(vector <int> a, vector <int> b);
-void ss(vector <int> a, vector <int> b);
-void pb(vector <int> a, vector <int> b);
+void sa(vector<int> &a);
+void sb(vector<int> &b);
+void ss(vector<int> &a, vector<int> &b);
 
+void pb(vector<int> a, vector <int> b);
 
+void ra(vector<int> &a);
+void rb(vector<int> &b);
+void rr(vector<int> &a, vector<int> &b);
+
+void rra(vector<int> &a);
+void rrb(vector<int> &b);
+void rrr(vector<int> &a, vector<int> &b);
 
 
 #endif // SORTIC_H_INCLUDED
